e1acf0245dac006042a83c0e6b104fb7768ed7c8
j-kiran <007jkiran@gmail.com>
2020-02-28 20:43:12 +0530

    prepare for deploy

---
+++ config/db.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+// const connectDB = () => {
+//     mongoose.connect(db, {
+//         useNewUrlParser: true,
+//         useCreateIndex: true
+//     })
+//     .then(() => console.log('MonogDB Connected'))
+//     .catch(err => {
+//         console.error(err.message);
+//         process.exit(1);
+//     })
+// }
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDb Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
